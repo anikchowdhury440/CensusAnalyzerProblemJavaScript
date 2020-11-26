527e5149688dd10a7015881fe7bf6fe1bf4dc51f
anikchowdhury440 <anikchowdhury440@gmail.com>
2020-11-26 18:11:37 +0530

    Added: Test Case for incorrect file and incorrect file type

--- __main__/censusAnalyzer.js
+++ __main__/censusAnalyzer.js
@@ -2,6 +2,7 @@ const { rejects } = require('assert');
 const csv = require('csv-parser');
 const fs = require('fs');
 const { resolve } = require('path');
+const path = require('path');
 
 let censusData = []
 class CensusAnalyzer {
@@ -11,15 +12,25 @@ class CensusAnalyzer {
 
     loadStateCensusCSV() {
         return new Promise((resolve, rejects) => {
-            fs.createReadStream(this.filePath)
-                .pipe(csv())
-                .on('data', (data) => {
-                    censusData.push(data);
-                })
-                .on('end', () => {
-                    resolve(censusData.length);
-                });
-        })
+            
+            if(!fs.existsSync(this.filePath)) {
+                rejects(new Error('No Such File'));
+            }
+            else{
+                var ext = path.extname(this.filePath);
+                if(ext != '.csv'){
+                    rejects(new Error('Invalid File Type'));
+                }
+                fs.createReadStream(this.filePath)
+                    .pipe(csv())
+                    .on('data', (data) => {
+                        censusData.push(data);
+                    })
+                    .on('end', () => {
+                        resolve(censusData.length);
+                    });
+            }
+        })      
     }
 }
 
--- __tests__/censusAnalyzer.test.js
+++ __tests__/censusAnalyzer.test.js
@@ -1,9 +1,18 @@
 const CensusAnalyzer = require('../__main__/censusAnalyzer');
 const INDIA_STATE_CENSUS_CSV = "C:\\Users\\Anik Chowdhury\\Desktop\\JavaScriptProject\\CensusAnalyzerProblem\\__main__\\__resources__\\IndiaStateCensusData.csv";
-
+const INDIA_STATE_CENSUS_TXT = "C:\\Users\\Anik Chowdhury\\Desktop\\JavaScriptProject\\CensusAnalyzerProblem\\__main__\\__resources__\\IndiaStateCensusData.txt";
+const WRONG_FILE = "C:\\Users\\Anik Chowdhury\\Desktop\\JavaScriptProject\\CensusAnalyzerProblem\\__main__\\__resources__\\IndiaStateCensuData.csv";
 describe('testsForLoadIndiaStateCensusCSV', () => {
     test('givenStateCensusFile_EnsureNoOfRecordsMatches', () => {
         const censusAnalyzer = new CensusAnalyzer(INDIA_STATE_CENSUS_CSV);
         return expect(censusAnalyzer.loadStateCensusCSV()).resolves.toBe(29);
+    });
+    test('givenStateCensusFileIfIncorrect_ShouldThrowException', () => {
+        const censusAnalyzer = new CensusAnalyzer(WRONG_FILE);
+        return expect(censusAnalyzer.loadStateCensusCSV()).rejects.toThrow('No Such File');
+    });
+    test('givenStateCensusFileIfcorrect_ButTypeIncorrect_ShouldThrowException', () => {
+        const censusAnalyzer = new CensusAnalyzer(INDIA_STATE_CENSUS_TXT);
+        return expect(censusAnalyzer.loadStateCensusCSV()).rejects.toThrow('Invalid File Type');
     });
 });
