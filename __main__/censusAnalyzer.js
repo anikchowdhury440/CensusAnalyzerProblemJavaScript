9136c1a0840d45824df21fa1051341a2fa4f724b
anikchowdhury440 <anikchowdhury440@gmail.com>
2020-11-27 08:57:14 +0530

    Refactored: Refactor test cases and implementation

--- __main__/censusAnalyzer.js
+++ __main__/censusAnalyzer.js
@@ -7,72 +7,76 @@ const path = require('path');
 let indiaStateCensusData = []
 let indiaStateCodeData = []
 class CensusAnalyzer {
-    constructor(filePath) {
-        this.filePath = filePath;
+    constructor() {
+        
     }
 
-    loadIndiaStateCensusCSV() {
+    loadIndiaStateCensusCSV(filePath) {
         return new Promise((resolve, rejects) => {
             
-            if(!fs.existsSync(this.filePath)) {
+            if(!fs.existsSync(filePath)) {
                 rejects(new Error('No Such File'));
             }
             else {
-                var ext = path.extname(this.filePath);
+                var ext = path.extname(filePath);
                 if(ext != '.csv'){
                     rejects(new Error('Invalid File Type'));
                 }
-                fs.createReadStream(this.filePath)
-                    .pipe(csv())
-                    .on('headers', (header) => {
-                        if(header[0] != 'State' || header[1] != 'Population' || header[2] != 'AreaInSqKm' || header[3] != 'DensityPerSqKm') {
-                            rejects(new Error('Invalid Header'));
-                        }
-                    })
-                    .on('data', (data) => {
-                        if(data.State == '' || data.Population == '' || data.AreaInSqKm == '' || data.DensityPerSqKm == '') {
-                            rejects(new Error('Invalid Delimiter'));
-                        }
-                        else {
-                            indiaStateCensusData.push(data);
-                        }
-                    })
-                    .on('end', () => {
-                        resolve(indiaStateCensusData.length);
-                    });
+                else {
+                    fs.createReadStream(filePath)
+                        .pipe(csv())
+                        .on('headers', (header) => {
+                            if(header[0] != 'State' || header[1] != 'Population' || header[2] != 'AreaInSqKm' || header[3] != 'DensityPerSqKm') {
+                                rejects(new Error('Invalid Header'));
+                            }
+                        })
+                        .on('data', (data) => {
+                            if(data.State == '' || data.Population == '' || data.AreaInSqKm == '' || data.DensityPerSqKm == '') {
+                                rejects(new Error('Invalid Delimiter'));
+                            }
+                            else {
+                                indiaStateCensusData.push(data);
+                            }
+                        })
+                        .on('end', () => {
+                            resolve(indiaStateCensusData.length);
+                        });
+                }
             }
         })      
     }
 
-    loadIndiaStateCodeCSV() {
+    loadIndiaStateCodeCSV(filePath) {
         return new Promise((resolve, rejects) => {
             
-            if(!fs.existsSync(this.filePath)) {
+            if(!fs.existsSync(filePath)) {
                 rejects(new Error('No Such File'));
             }
             else {
-                var ext = path.extname(this.filePath);
+                var ext = path.extname(filePath);
                 if(ext != '.csv'){
                     rejects(new Error('Invalid File Type'));
                 }
-                fs.createReadStream(this.filePath)
-                    .pipe(csv())
-                    .on('headers', (header) => {
-                        if(header[0] != 'SrNo' || header[1] != 'StateName' || header[2] != 'TIN' || header[3] != 'StateCode') {
-                            rejects(new Error('Invalid Header'));
-                        }
-                    })
-                    .on('data', (data) => {
-                        if(data.SrNo == '' || data.StateName == '' || data.TIN == '' || data.StateCode == '') {
-                            rejects(new Error('Invalid Delimiter'));
-                        }
-                        else {
-                            indiaStateCodeData.push(data);
-                        }
-                    })
-                    .on('end', () => {
-                        resolve(indiaStateCodeData.length);
-                    });
+                else {
+                    fs.createReadStream(filePath)
+                        .pipe(csv())
+                        .on('headers', (header) => {
+                            if(header[0] != 'SrNo' || header[1] != 'StateName' || header[2] != 'TIN' || header[3] != 'StateCode') {
+                                rejects(new Error('Invalid Header'));
+                            }
+                        })
+                        .on('data', (data) => {
+                            if(data.SrNo == '' || data.StateName == '' || data.TIN == '' || data.StateCode == '') {
+                                rejects(new Error('Invalid Delimiter'));
+                            }
+                            else {
+                                indiaStateCodeData.push(data);
+                            }
+                        })
+                        .on('end', () => {
+                            resolve(indiaStateCodeData.length);
+                        });
+                }
             }
         })      
     }
--- __tests__/censusAnalyzer.test.js
+++ __tests__/censusAnalyzer.test.js
@@ -10,54 +10,54 @@ const INDIA_STATE_CODE_WRONG_DELIMITER_CSV = "C:\\Users\\Anik Chowdhury\\Desktop
 
 describe('testsForLoadIndiaStateCensusCSV', () => {
     test('givenIndiaStateCensusFile_EnsureNoOfRecordsMatches', () => {
-        const censusAnalyzer = new CensusAnalyzer(INDIA_STATE_CENSUS_CSV);
-        return expect(censusAnalyzer.loadIndiaStateCensusCSV()).resolves.toBe(29);
+        const censusAnalyzer = new CensusAnalyzer();
+        return expect(censusAnalyzer.loadIndiaStateCensusCSV(INDIA_STATE_CENSUS_CSV)).resolves.toBe(29);
     });
     
     test('givenIndiaStateCensusFileIfIncorrect_ShouldThrowException', () => {
-        const censusAnalyzer = new CensusAnalyzer(WRONG_FILE);
-        return expect(censusAnalyzer.loadIndiaStateCensusCSV()).rejects.toThrow('No Such File');
+        const censusAnalyzer = new CensusAnalyzer();
+        return expect(censusAnalyzer.loadIndiaStateCensusCSV(WRONG_FILE)).rejects.toThrow('No Such File');
     });
     
     test('givenIndiaStateCensusFileIfCorrect_ButTypeIncorrect_ShouldThrowException', () => {
-        const censusAnalyzer = new CensusAnalyzer(INDIA_STATE_CENSUS_TXT);
-        return expect(censusAnalyzer.loadIndiaStateCensusCSV()).rejects.toThrow('Invalid File Type');
+        const censusAnalyzer = new CensusAnalyzer();
+        return expect(censusAnalyzer.loadIndiaStateCensusCSV(INDIA_STATE_CENSUS_TXT)).rejects.toThrow('Invalid File Type');
     });
     
     test('givenIndiaStateCensusFileIfCorrect_ButDelimitterIncorrect_ShouldThrowException', () => {
-        const censusAnalyzer = new CensusAnalyzer(INDIA_STATE_CENSUS_WRONG_DELIMITER_CSV);
-        return expect(censusAnalyzer.loadIndiaStateCensusCSV()).rejects.toThrow('Invalid Delimiter');
+        const censusAnalyzer = new CensusAnalyzer();
+        return expect(censusAnalyzer.loadIndiaStateCensusCSV(INDIA_STATE_CENSUS_WRONG_DELIMITER_CSV)).rejects.toThrow('Invalid Delimiter');
     });
 
     test('givenIndiaStateCensusFileIfCorrect_ButHeaderIncorrect_ShouldThrowException', () => {
-        const censusAnalyzer = new CensusAnalyzer(INDIA_STATE_CODE_CSV);
-        return expect(censusAnalyzer.loadIndiaStateCensusCSV()).rejects.toThrow('Invalid Header');
+        const censusAnalyzer = new CensusAnalyzer();
+        return expect(censusAnalyzer.loadIndiaStateCensusCSV(INDIA_STATE_CODE_CSV)).rejects.toThrow('Invalid Header');
     });
 });
 
 describe('testsForLoadIndiaStateCodeCSV', () => {
     test('givenIndiaStateCodeFile_EnsureNoOfRecordsMatches', () => {
-        const censusAnalyzer = new CensusAnalyzer(INDIA_STATE_CODE_CSV);
-        return expect(censusAnalyzer.loadIndiaStateCodeCSV()).resolves.toBe(37);
+        const censusAnalyzer = new CensusAnalyzer();
+        return expect(censusAnalyzer.loadIndiaStateCodeCSV(INDIA_STATE_CODE_CSV)).resolves.toBe(37);
     });
     
     test('givenIndiaStateCodeFileIfIncorrect_ShouldThrowException', () => {
-        const censusAnalyzer = new CensusAnalyzer(WRONG_FILE);
-        return expect(censusAnalyzer.loadIndiaStateCodeCSV()).rejects.toThrow('No Such File');
+        const censusAnalyzer = new CensusAnalyzer();
+        return expect(censusAnalyzer.loadIndiaStateCodeCSV(WRONG_FILE)).rejects.toThrow('No Such File');
     });
     
     test('givenIndiaStateCodeFileIfCorrect_ButTypeIncorrect_ShouldThrowException', () => {
-        const censusAnalyzer = new CensusAnalyzer(INDIA_STATE_CODE_TXT);
-        return expect(censusAnalyzer.loadIndiaStateCodeCSV()).rejects.toThrow('Invalid File Type');
+        const censusAnalyzer = new CensusAnalyzer();
+        return expect(censusAnalyzer.loadIndiaStateCodeCSV(INDIA_STATE_CODE_TXT)).rejects.toThrow('Invalid File Type');
     });
     
     test('givenStateCodeFileIfCorrect_ButDelimitterIncorrect_ShouldThrowException', () => {
-        const censusAnalyzer = new CensusAnalyzer(INDIA_STATE_CODE_WRONG_DELIMITER_CSV);
-        return expect(censusAnalyzer.loadIndiaStateCodeCSV()).rejects.toThrow('Invalid Delimiter');
+        const censusAnalyzer = new CensusAnalyzer();
+        return expect(censusAnalyzer.loadIndiaStateCodeCSV(INDIA_STATE_CODE_WRONG_DELIMITER_CSV)).rejects.toThrow('Invalid Delimiter');
     });
 
     test('givenStateCodeFileIfCorrect_ButHeaderIncorrect_ShouldThrowException', () => {
-        const censusAnalyzer = new CensusAnalyzer(INDIA_STATE_CENSUS_CSV);
-        return expect(censusAnalyzer.loadIndiaStateCodeCSV()).rejects.toThrow('Invalid Header');
+        const censusAnalyzer = new CensusAnalyzer();
+        return expect(censusAnalyzer.loadIndiaStateCodeCSV(INDIA_STATE_CENSUS_CSV)).rejects.toThrow('Invalid Header');
     });
 });
